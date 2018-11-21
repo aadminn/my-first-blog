cf9129858df44d7950c32830e274b73711fbee13
admin20181 <andrerabelo200118@hotmail.com>
2018-11-21 09:18:02 -0300

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
@@ -11,6 +10,7 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
 
 def post_new(request):
     if request.method == "POST":
